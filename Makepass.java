5e57ba472bd2a6b9521e3c64534b3ad3bec5e4db
h0tcat <h0tcat>
2021-01-22 13:19:17 +0900

    edit Indent

--- Makepass.java
+++ Makepass.java
@@ -12,7 +12,7 @@ public class Makepass
 	public static void main(String[] args)
 	{
 		Scanner scan = new Scanner(System.in);
-		
+
 		//定数を宣言（Asciiコード）
 		final int Asc_exc = '!';
 		final int Asc_0 = '0';
@@ -37,18 +37,18 @@ public class Makepass
 				continue;
 			}
 		}
-		
+
 		System.out.println("記号：有_y   無_n");
 		mark = scan.next();
 		scan.close();
 
 		StringBuilder pass = new StringBuilder();
-		
+
 		Random random = new Random();
 		//記号ありのパスワード生成
-		
+
 		if(mark.equals("y") || mark.equals("Y")){
-			
+
 			for(int j=0;j<len;j++){	
 				for(int i=0;i<len;i++){
 					int rand = random.nextInt(Asc_tilde) + Asc_exc;
@@ -59,27 +59,27 @@ public class Makepass
 		//記号なしのパスワード生成
 		else if(mark.equals("n") || mark.equals("N")){
 			boolean isAscii;
-			
+
 			for(int j=0;j<4;j++){	
 				for(int i=0;i<len;i++){
 					int rand = random.nextInt(Asc_z) + Asc_0;
 					isAscii = rand >= Asc_0 && rand <= Asc_9 || rand >= Asc_A && rand <= Asc_Z || rand >= Asc_a && rand <= Asc_z;
-					
+
 					if(isAscii)
 						pass.append((char)rand);
 				};
 			}
 		}		
-		
+
 		try {
 			byte[] passBytes = pass.toString().getBytes(StandardCharsets.UTF_8);
 			Encoder base64Encoder=Base64.getEncoder();
-			
+
 			String result=new String(base64Encoder.encode(passBytes),StandardCharsets.UTF_8);
 			System.out.println(result);
-    } catch (Exception e){
-      e.printStackTrace();
-    }
-		
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+
 	}
 }
