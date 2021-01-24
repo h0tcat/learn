bdfccec8525caa387e1ea56967f204a48c376a49
h0tcat <h0tcat@vivaldi.net>
2021-01-24 10:07:42 +0900

    if文を完結にしました

--- rot13/Rot13.cpp
+++ rot13/Rot13.cpp
@@ -7,25 +7,29 @@ nori::Rot13::Rot13(){
 std::string nori::Rot13::Convert(char* plainText){
 	
 	this->wordSize=strlen(plainText);
-	char wordBuffer[2];
-
+	 
 	for(int i=0;i<this->wordSize;i++){
 		this->wordAsciiCode = plainText[i];
+		this->wordAsciiCode>='A' && this->wordAsciiCode<='Z';
 
-		if(this->wordAsciiCode>='A' && this->wordAsciiCode<='Z'){
+
+		this->isAtoZ = this->wordAsciiCode>='A' && this->wordAsciiCode<='Z';
+		this->is_aToz = this->wordAsciiCode>='a' && this->wordAsciiCode<='z';
+
+		if(this->isAtoZ){
 			this->wordAsciiCode += 13;
 
 			if(this->wordAsciiCode >= '[')
 				this->wordAsciiCode-=26;
-		}else if(this->wordAsciiCode>='a' && this->wordAsciiCode<='z'){
+		}else if(this->is_aToz){
 			this->wordAsciiCode += 13;
 			if(this->wordAsciiCode>='{')
 				this->wordAsciiCode -= 26;
 		}
-		wordBuffer[0]=(char)this->wordAsciiCode;
-		wordBuffer[1]='\0';
+		this->wordBuffer[0]=(char)this->wordAsciiCode;
+		this->wordBuffer[1]='\0';
 
-		this->word.append(wordBuffer);
+		this->word.append(this->wordBuffer);
 
 	}
 	return this->word;
--- rot13/Rot13.h
+++ rot13/Rot13.h
@@ -9,8 +9,12 @@ namespace nori{
 	class Rot13{
 		private:
 			std::string word;
+			char wordBuffer[2];
+
 			int wordAsciiCode;
 			size_t wordSize;
+
+			bool isAtoZ,is_aToz;
 		public:
 			Rot13();
 			std::string Convert(char* plainText);
